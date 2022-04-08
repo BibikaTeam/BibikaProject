df9bed327638241014a6237263d366492fdd4104
r3ch1953 <example@mail.com>
2022-04-08 19:32:09 +0300

    Divided the Engine into two properties. Moved out GearBox in a separate table.

---
+++ BibikaProject.Domain/Entities/Core/GearBox.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BibikaProject.Domain.Entities.Core
+{
+    public class GearBox
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
