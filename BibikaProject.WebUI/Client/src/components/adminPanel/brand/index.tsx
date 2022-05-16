23b869aa5d7d114bece8696d45397e8afe378054
CoolSanya <Zhyhy123@gmail.com>
2022-05-16 16:09:47 +0300

    add edit modal window

--- BibikaProject.WebUI/Client/src/components/adminPanel/brand/index.tsx
+++ BibikaProject.WebUI/Client/src/components/adminPanel/brand/index.tsx
@@ -17,43 +17,78 @@ const BrandPage = () => {
 
   const [form] = Form.useForm();
 
-  const dataSource = [ initialValues ];
+  const dataSource = [initialValues];
 
 
   const columns = [
     {
       title: "Id",
-      dataIndex: "id"
+      dataIndex: "id",
+      key: "id"
     },
     {
       title: "Назва",
-      dataIndex: "title"
+      dataIndex: "title",
+      key: "title"
     },
-    // {
-    //   title: "Дії",
-    //   dataIndex: "actions",
-    //   render: (a: any, record: IBrandModel) => {
-    //     <div className="buttonGroup">
-    //       <Popconfirm
-    //         title={`Ви впевнені що хочете видалити ${record.title}?`}
-    //         onConfirm={() => handleDeleteBrand(record)} >
-    //         <Button htmlType="button" type="default" className="buttonDanger">
-    //           Видалити
-    //         </Button>
-    //       </Popconfirm>
-    //       <Popconfirm
-    //         title={"Редагування марки"} >
-    //         <Button htmlType="button" type="default" className="buttonInfo">
-    //           Редагувати
-    //         </Button>
-    //       </Popconfirm>
-    //     </div>
-    //   }
-    // }
+    {
+      title: "Дії",
+      dataIndex: "actions",
+      key: "actions",
+      render: (record: IBrandModel) => (
+        <div className="buttonGroup">
+          <Button htmlType="submit" type="default" className="buttonInfo" onClick={showModalUpdateBrand}>
+            Редагувати
+          </Button>
+          <Modal
+            title="Редагувавання марки авто" visible={isModalVisible}
+            onCancel={handleCloseModalBrand} footer={false}
+          >
+            <Form
+              name="basic"
+              labelCol={{ span: 10 }}
+              wrapperCol={{ span: 16 }}
+              //onFinish={handleOkModalAddNewBrand}
+              autoComplete="off"
+            >
+              <Form.Item
+                label="Зміна назви марки машини"
+                name="title"
+                rules={[{ required: true, message: 'Введіть нову назву марки машини'}]}
+              >
+                <Input />
+              </Form.Item>
+
+              <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                <Button type="default" htmlType="submit" onClick={handleCloseModalBrand}>
+                  Відмінити
+                </Button>
+                &nbsp;
+                <Button type="primary" htmlType="submit">
+                  Змінити
+                </Button>
+              </Form.Item>
+            </Form>
+
+          </Modal>
+          &nbsp;
+          <Popconfirm
+            title={`Ви впевнені що хочете видалити цю марку?`}
+          //title={`Ви впевнені що хочете видалити ${record.title }?`}
+          //onConfirm={() => handleDeleteBrand(record)} 
+          >
+            <Button type="primary" htmlType="submit" className="danger">
+              Видалити
+            </Button>
+          </Popconfirm>
+
+        </div>
+      )
+    }
   ];
 
   useEffect(() => {
-      handleGetAllBrands();
+    handleGetAllBrands();
   }, []);
 
   const handleGetAllBrands = async () => {
@@ -87,6 +122,23 @@ const BrandPage = () => {
     }
   };
 
+  const handleUpdateBrand = async (
+    value: IBrandModel
+  ) => {
+    setLoading(true);
+    try {
+      await updateBrand(value);
+      toast.success(`Brand ${value.title} are successfully update`);
+    } catch (error) {
+      const errorType = error as BrandErrorType;
+      errorType.errorsString.forEach((el) => {
+        toast.error(el);
+      });
+    } finally {
+      setLoading(false);
+    }
+  }
+
   const handleDeleteBrand = async (
     value: IBrandModel
   ) => {
@@ -106,67 +158,73 @@ const BrandPage = () => {
 
 
 
-  const showModalAddNewBrand = () => { setIsModalVisible(true)};
+  const showModalAddNewBrand = () => { setIsModalVisible(true) };
 
   const handleOkModalAddNewBrand = (value: IBrandModel) => {
     handleAddBrand(value);
     setIsModalVisible(false);
   }
 
-  const handleCloseModalAddNewBrand = () => {
+  const showModalUpdateBrand = () => {
+    setIsModalVisible(true) 
+  };
 
+  const handleOkModalUpdateBrand = (value: IBrandModel) => {
+    handleUpdateBrand(value);
     setIsModalVisible(false);
   }
 
+  const handleCloseModalBrand = () => {
+    setIsModalVisible(false);
+  }
+
+  
 
   return (
     <div>
       {loading}
       <div className="text-align: center">
-      <Button htmlType="button" type="default" className="buttonPrimary" onClick={showModalAddNewBrand} >
-              Додати нову марку авто
-      </Button>
-      <Modal 
-        title="Додавання нової марки авто" visible={isModalVisible}
-        onCancel={handleCloseModalAddNewBrand} footer={false}
-      >
-
-
-
-<Form
-      name="basic"
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 16 }}
-      onFinish={handleOkModalAddNewBrand}
-      autoComplete="off"
-    >
-      <Form.Item
-        label="Назва марки"
-        name="title"
-        rules={[{ required: true, message: 'Введіть нову марку машини' }]}
-      >
-        <Input />
-      </Form.Item>
-
-      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-        <Button type="primary" htmlType="submit">
-          Додати
+        <Button htmlType="button" type="default" className="buttonPrimary" onClick={showModalAddNewBrand} >
+          Додати нову марку авто
         </Button>
-        <Button type="primary" htmlType="submit" onClick={handleCloseModalAddNewBrand}>
-          Відмінити
-        </Button>
-      </Form.Item>
-    </Form>
+        <Modal
+          title="Додавання нової марки авто" visible={isModalVisible}
+          onCancel={handleCloseModalBrand} footer={false}
+        >
+          <Form
+            name="basic"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            onFinish={handleOkModalAddNewBrand}
+            autoComplete="off"
+          >
+            <Form.Item
+              label="Назва марки"
+              name="title"
+              rules={[{ required: true, message: 'Введіть нову марку машини' }]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+              <Button type="default" htmlType="submit" onClick={handleCloseModalBrand}>
+                Відмінити
+              </Button>
+              &nbsp;
+              <Button type="primary" htmlType="submit">
+                Додати
+              </Button>
+            </Form.Item>
+          </Form>
 
-      </Modal>
+        </Modal>
       </div>
       <Table
-        size = "large"
+        size="large"
         dataSource={dataSource}
         columns={columns}
         rowKey="id"
         pagination={false}
-        />
+      />
     </div>
   );
 }
