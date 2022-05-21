a6f1d2d71257f7bc329e44c937e015fd465e27bf
Kolya Koval <kovalkola2@gmail.com>
2022-05-21 12:22:34 +0200

    Bug fix

--- BibikaProject.WebUI/Client/src/components/adminPanel/brand/index.tsx
+++ BibikaProject.WebUI/Client/src/components/adminPanel/brand/index.tsx
@@ -3,33 +3,32 @@ import { Id, toast } from "react-toastify";
 import { BrandErrorType, IBrandModel } from "../types";
 import { Link } from "react-router-dom";
 
-import { Input, Form, Checkbox, Button, Popconfirm, Table, Modal } from "antd";
+import { FormModal } from "../../common/form";
+
+import { Input, Form, Checkbox, Button, Popconfirm, Table } from "antd";
 import { getAllBrands, addBrand, updateBrand, deleteBrand } from "./service";
 
 const BrandPage = () => {
   const [loading, setLoading] = useState<boolean>(false);
-  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isModalAdd, setModalAdd] = useState(false);
+  const [isModalEdit, setModalEdit] = useState(false);
+  const [brands, setBrands] = useState<Array<IBrandModel>>([]);
   //const [inputAddBrandValue, setInputAddBrandValue] = useState<IBrandModel>
-  const initialValues: IBrandModel = {
-    id: 1,
-    title: "Ford",
-  };
-
-  const tmpBrand: IBrandModel = {
-    id: 2,
-    title: "jet",
-  };
-
   const [form] = Form.useForm();
 
   useEffect(() => {
-    handleGetAllBrands();
-  }, [loading]);
+    const init = async () => {
+      await handleGetAllBrands();
+    };
+    init();
+  }, []);
 
   const handleGetAllBrands = async () => {
     setLoading(true);
     try {
-      return await getAllBrands();
+      await getAllBrands().then((data) => {
+        setBrands(data);
+      });
     } catch (error) {
       const errorType = error as BrandErrorType;
       errorType.errorsString.forEach((el) => {
@@ -38,24 +37,9 @@ const BrandPage = () => {
     } finally {
       setLoading(false);
     }
-  }
-  console.log("handle get all ");
-  
-  console.log(handleGetAllBrands());
+  };
 
-  const dataSource = [
-    handleGetAllBrands()
-  ];
-  // console.log("dataSource");
-  
-  // console.log(dataSource);
-  
-
-  
-  
-  const handleAddBrand = async (
-    values: IBrandModel,
-  ) => {
+  const handleAddBrand = async (values: IBrandModel) => {
     setLoading(true);
     try {
       await addBrand(values);
@@ -69,10 +53,7 @@ const BrandPage = () => {
       setLoading(false);
     }
   };
-
-  const handleUpdateBrand = async (
-    value: IBrandModel
-  ) => {
+  const handleUpdateBrand = async (value: IBrandModel) => {
     setLoading(true);
     try {
       await updateBrand(value);
@@ -85,15 +66,15 @@ const BrandPage = () => {
     } finally {
       setLoading(false);
     }
-  }
-
-  const handleDeleteBrand = async (
-    value: IBrandModel
-  ) => {
+  };
+  const handleDeleteBrand = async (value: IBrandModel) => {
+    console.log("value: ", value);
     setLoading(true);
     try {
       await deleteBrand(value.id);
-      toast.warning(`Brand ${value.title} are successfully deleted`);
+      toast.success(`Brand ${value.title} are successfully deleted`);
+
+      setBrands(brands.filter((x) => x.id != value.id));
     } catch (error) {
       const errorType = error as BrandErrorType;
       errorType.errorsString.forEach((el) => {
@@ -102,47 +83,33 @@ const BrandPage = () => {
     } finally {
       setLoading(false);
     }
-  }
-
-  //handleGetAllBrands().then(d=> { console.log(d);});
-
-  // const showModalUpdateBrand = () => {
-  //   setIsModalVisible(true) 
-  // };
-
-  // const handleOkModalUpdateBrand = (value: IBrandModel) => {
-  //   handleUpdateBrand(value);
-  //   setIsModalVisible(false);
-  // }
-
-  const handleCloseModalBrand = () => {
-    setIsModalVisible(false);
-  }
-
+  };
 
   const columns = [
     {
       title: "Id",
       dataIndex: "id",
-      key: "id"
+      key: "id",
     },
     {
       title: "Назва",
       dataIndex: "title",
-      key: "title"
+      key: "title",
     },
     {
       title: "Дії",
       dataIndex: "actions",
       key: "actions",
-      render: (record: IBrandModel) => (
+      render: (text: string, record: IBrandModel) => (
         <div className="buttonGroup">
           <Button htmlType="submit" type="default" className="buttonInfo">
             Редагувати
           </Button>
-          <Modal
-            title="Редагувавання марки авто" visible={isModalVisible}
-            onCancel={handleCloseModalBrand} footer={false}
+          <FormModal
+            title="Редагувавання марки авто"
+            visible={isModalEdit}
+            onCancel={() => setModalEdit(false)}
+            onSubmit={() => {}}
           >
             <Form
               name="basic"
@@ -154,93 +121,102 @@ const BrandPage = () => {
               <Form.Item
                 label="Зміна назви марки машини"
                 name="title"
-                rules={[{ required: true, message: 'Введіть нову назву марки машини'}]}
+                rules={[
+                  {
+                    required: true,
+                    message: "Введіть нову назву марки машини",
+                  },
+                ]}
               >
-                <Input/>
+                <Input />
               </Form.Item>
-              <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-                <Button type="default" htmlType="submit" onClick={handleCloseModalBrand}>
-                  Відмінити
-                </Button>
-                &nbsp;
-                <Button type="primary" htmlType="submit">
-                  Змінити
-                </Button>
-              </Form.Item>
+              <Form.Item wrapperCol={{ offset: 8, span: 16 }}></Form.Item>
             </Form>
-
-          </Modal>
+          </FormModal>
           &nbsp;
           <Popconfirm
-            //title={`Ви впевнені що хочете видалити цю марку?`}
-          title={`Ви впевнені що хочете видалити ${tmpBrand.title }?`}
-          //onConfirm={() => handleDeleteBrand(record)} 
+            title={`Ви впевнені що хочете видалити цю марку?`}
+            onConfirm={() => handleDeleteBrand(record)}
           >
             <Button type="primary" htmlType="submit" className="danger">
               Видалити
             </Button>
           </Popconfirm>
-
         </div>
-      )
-    }
+      ),
+    },
   ];
 
-  
-  const showModalAddNewBrand = () => { setIsModalVisible(true) };
+  const showModalAddNewBrand = () => {
+    setModalAdd(true);
+  };
 
-  const handleOkModalAddNewBrand = (value: IBrandModel) => {
+  const handleOkModalAddNewBrand = () => {
+    form.submit();
+    setModalAdd(false);
+  };
+  const handleFormSubmit = (value: IBrandModel) => {
     handleAddBrand(value);
-    setIsModalVisible(false);
-  }
+  };
 
   return (
     <div>
       {loading}
       <div className="text-align: center">
-        <Button htmlType="button" type="default" className="buttonPrimary" onClick={showModalAddNewBrand} >
+        <Button
+          htmlType="button"
+          type="default"
+          className="buttonPrimary"
+          onClick={showModalAddNewBrand}
+        >
           Додати нову марку авто
         </Button>
-        <Modal
-          title="Додавання нової марки авто" visible={isModalVisible}
-          onCancel={handleCloseModalBrand} footer={false}
+        <FormModal
+          title="Додавання нової марки авто"
+          visible={isModalAdd}
+          onCancel={() => setModalAdd(false)}
+          onSubmit={handleOkModalAddNewBrand}
         >
           <Form
             name="basic"
             labelCol={{ span: 8 }}
             wrapperCol={{ span: 16 }}
-            onFinish={handleOkModalAddNewBrand}
+            onFinish={handleFormSubmit}
             autoComplete="off"
+            form={form}
           >
             <Form.Item
               label="Назва марки"
               name="title"
-              rules={[{ required: true, message: 'Введіть нову марку машини' }]}
+              rules={[{ required: true, message: "Введіть нову марку машини" }]}
             >
               <Input />
             </Form.Item>
             <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-              <Button type="default" htmlType="submit" onClick={handleCloseModalBrand}>
+              {/* <Button
+                type="default"
+                htmlType="submit"
+                onClick={handleCloseModalBrand}
+              >
                 Відмінити
               </Button>
               &nbsp;
               <Button type="primary" htmlType="submit">
                 Додати
-              </Button>
+              </Button> */}
             </Form.Item>
           </Form>
-
-        </Modal>
+        </FormModal>
       </div>
       <Table
         size="large"
-        dataSource={dataSource}
+        dataSource={brands}
         columns={columns}
         rowKey="id"
         pagination={false}
       />
     </div>
   );
-}
+};
 
 export default BrandPage;
--- BibikaProject.WebUI/Client/src/components/common/form/index.tsx
+++ BibikaProject.WebUI/Client/src/components/common/form/index.tsx
@@ -1,5 +1,7 @@
 import FormInput from "./input";
 import FormButton from "./button";
+import FormModal from "./modal";
 
 export { FormInput };
 export { FormButton };
+export { FormModal };
