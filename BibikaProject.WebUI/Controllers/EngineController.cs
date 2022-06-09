415df0b0cb35112ce198199b52376fce9803ed9f
r3ch1953 <eduardbernasky@gmail.com>
2022-06-09 15:10:49 +0300

    Added Paged Engine to contorller.

--- BibikaProject.WebUI/Controllers/EngineController.cs
+++ BibikaProject.WebUI/Controllers/EngineController.cs
@@ -1,4 +1,5 @@
 ﻿using BibikaProject.Application.Core.DTO.Engine;
+using BibikaProject.Application.Core.Requests;
 using BibikaProject.Application.Core.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -44,8 +45,16 @@ namespace BibikaProject.WebUI.Controllers
             return Ok();
         }
 
+        [HttpGet("get")]
+        public async Task<IActionResult> GetEngines([FromQuery] PagedEngineRequest request)
+        {
+            var result = await engineService.GetPagedEnginesAsync(request);
+
+            return Ok(result);
+        }
+
         [HttpGet("get/all")]
-        public async Task<IActionResult> GetAllGenrations()
+        public async Task<IActionResult> GetAllEngines()
         {
             var result = await engineService.GetAllEnginesAsync();
 
@@ -53,7 +62,7 @@ namespace BibikaProject.WebUI.Controllers
         }
 
         [HttpGet("get/by-generation/{id}")]
-        public async Task<IActionResult> GetGenerationsByBrand(int id)
+        public async Task<IActionResult> GetEnginesByGeneration(int id)
         {
             var result = await engineService.GetAllEnginesByGenerationAsync(id);
 
