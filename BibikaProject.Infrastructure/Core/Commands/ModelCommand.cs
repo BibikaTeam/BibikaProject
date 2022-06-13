661feef455c1e8b5535b0067fb33d979221bd14d
r3ch1953 <eduardbernasky@gmail.com>
2022-06-13 22:46:09 +0300

    Added error handling to wrong id in Model add and Generation add. Formated FluentValidation output to project standart.

--- BibikaProject.Infrastructure/Core/Commands/ModelCommand.cs
+++ BibikaProject.Infrastructure/Core/Commands/ModelCommand.cs
@@ -1,5 +1,7 @@
 ﻿using BibikaProject.Application.Core.Commands;
 using BibikaProject.Domain.Entities.Core;
+using BibikaProject.Infrastructure.Core.Errors;
+using System.Threading.Tasks;
 
 namespace BibikaProject.Infrastructure.Core.Commands
 {
@@ -7,6 +9,16 @@ namespace BibikaProject.Infrastructure.Core.Commands
     {
         public ModelCommand(ApplicationDbContext context) : base(context)
         {
+        }
+
+        public override async Task<Model> AddAsync(Model entity)
+        {
+            if (await context.Brands.FindAsync(entity.BrandId) == null)
+            {
+                throw new BadRequestException("There is no brand with this Id.");
+            }
+
+            return await base.AddAsync(entity);
         }
     }
 }
---
+++ BibikaProject.Infrastructure/Core/Errors/BadRequestException.cs
@@ -0,0 +1,19 @@
+﻿using BibikaProject.Infrastructure.Errors;
+using System.Net;
+
+namespace BibikaProject.Infrastructure.Core.Errors
+{
+    public class BadRequestException : BaseException
+    {
+        public BadRequestException(string[] errors) : base(errors)
+        {
+            Code = HttpStatusCode.BadRequest;
+        }
+
+        public BadRequestException(string error) : base(new string[] { error })
+        {
+            Code = HttpStatusCode.BadRequest;
+        }
+
+    }
+}
