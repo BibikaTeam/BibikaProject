b538c68ec5abb5684eec343ba884d849405de476
r3ch1953 <eduardbernasky@gmail.com>
2022-06-13 23:32:46 +0300

    Update ExceptionMiddleware.

--- BibikaProject.WebUI/ExceptionMiddleware/ErrorDetails.cs
+++ BibikaProject.WebUI/ExceptionMiddleware/ErrorDetails.cs
@@ -4,8 +4,8 @@ namespace BibikaProject.WebUI.ExceptionMiddleware
 {
     internal class ErrorDetails
     {
-        public int StatusCode { get; set; }
-        public string Message { get; set; }
+        public int Code { get; set; }
+        public string[] Errors { get; set; }
 
         public override string ToString() => JsonSerializer.Serialize(this);
     }
