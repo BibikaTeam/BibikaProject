79100929fcd370309ee214bd629668e745572021
r3ch1953 <eduardbernatskyi@proton.me>
2022-08-16 12:14:39 +0300

    Added interface for advertisment service

---
+++ BibikaProject.Application/Core/Services/IAdvertismentService.cs
@@ -0,0 +1,29 @@
+﻿using BibikaProject.Application.Core.DTO.Post;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BibikaProject.Application.Core.Services
+{
+    public interface IAdvertismentService
+    {
+        Task<PostDTO> GetRandomBannerPost();
+
+        Task<PostDTO> GetRandomTrendPost();
+
+        Task<List<PostDTO>> GetRandomTrendPosts(); // returns 3 random trend posts
+
+        Task EnableBannerAdvForPost(int postId, int addViews);
+
+        Task DisableBannerAdvForPost(int postId);
+
+        Task EnableTrendAdvForPost(int postId, int addViews);
+
+        Task DisableTrendAdvForPost(int postId);
+
+        Task AddMoneyToBalance(int postId, float amount);
+
+        Task AddBannerViews(int postId, int amount);
+
+        Task AddTrendViews(int postId, int amount);
+    }
+}
