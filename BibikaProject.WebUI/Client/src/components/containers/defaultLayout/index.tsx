e8b3dcc5a37101ee016d9ad9e66b24b795e1a997
r3ch1953 <eduardbernatskyi@proton.me>
2022-08-17 11:23:45 +0300

    Added footer

--- BibikaProject.WebUI/Client/src/components/containers/defaultLayout/index.tsx
+++ BibikaProject.WebUI/Client/src/components/containers/defaultLayout/index.tsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router";
+import Footer from "./footer";
 import Header from "./header";
 
 const DefaultLayout = () => {
@@ -6,6 +7,7 @@ const DefaultLayout = () => {
     <>
       <Header />
       <Outlet></Outlet>
+      <Footer />
     </>
   );
 };
