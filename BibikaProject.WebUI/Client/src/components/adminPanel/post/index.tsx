7157c6cb1f17f34f15266119be6a37a617de6714
Саня Жигула <zhyhy123@gmail.com>
2022-09-06 23:48:56 +0300

    Add modal advertising

--- BibikaProject.WebUI/Client/src/components/adminPanel/post/index.tsx
+++ BibikaProject.WebUI/Client/src/components/adminPanel/post/index.tsx
@@ -1,10 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import {
-    IBrandModel,
-    IPaginationBrandModel,
-    IPaginationBrandRequest,
-    IPaginationModel,
     IRequestError,
 } from "../types";
 
@@ -27,76 +23,68 @@ const Context = React.createContext({ name: "Default" });
 
 const AdminPanelPostPage = () => {
     const [loading, setLoading] = useState<boolean>(false);
-    const [isModalAdd, setModalAdd] = useState(false);
-    const [paginatedBrands, setPaginatedBrands] =
-        useState<IPaginationBrandRequest>({
-            allPages: 0,
-            currentPage: 0,
-            data: [],
-        });
-    const countOnPage: number = 10;
-
+    const [isModalAdvertising, setModalAdvertising] = useState(false);
+    const [userPosts, setUserPosts] = useState([]);
     const [form] = Form.useForm();
 
     let key = ``;
 
     const [api, contextHolder] = notification.useNotification();
 
-    useEffect(() => {
-        const init = async () => {
-        };
-        init();
-    }, []);
-
-    const handleGetAllPostsUser = async () => {
-        const userEmail = "vaxodi1964@qqhow.com";
+    const handleGetAllPostsUser = async (value: string) => {
         setLoading(true);
         try {
-            console.log("handle user email", userEmail);
+            const userPost = await getUserPostEmail(value);
+            setUserPosts(userPost);
+            console.log("user posts", userPosts);
             
-            const userPost = await getUserPostEmail(userEmail);
-
-            console.log("user post", userPost);
-            
-
         } catch (_error) {
             const error: IRequestError = _error as IRequestError;
             error.errors.forEach((e) => {
-              toast.error(e);
+                toast.error(e);
             });
-          } finally {
+        } finally {
             setLoading(false);
             notification.close(key);
-          }
+        }
     }
+
+    const showModalAdvertising = () => {
+        setModalAdvertising(true);
+      };
+
+    const handleSearchUserEmailChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+        const userEmail = e.target.value;
+        await handleGetAllPostsUser(userEmail);
+      };
 
     const columns = [
         {
             title: "Назва посту",
-            dataIndex: "title",
-            key: "title",
-            outerWidth: "30%",
+            dataIndex: "sellerName",
+            key: "sellerName",
+            outerWidth: "60%",
         },
         {
             title: "Дії",
             dataIndex: "actions",
             key: "actions",
-            outerWidth: "70%",
-            render: (text: string, record: IBrandModel) => (
+            outerWidth: "40%",
+            render: (text: string) => (
                 <div className="buttonGroup">
-                    <Button type="primary" htmlType="submit" className="danger">
+                    <Button type="primary" htmlType="submit" className="danger" style={{ marginRight: 20 }}>
                         Переглянути пост
                     </Button>
                     <Popconfirm
-                        title={`Ви впевнені що хочете видалити цю марку?`}
+                        title={`Ви впевнені що хочете видалити цей пост?`}
                     //onConfirm={() => handleDeleteBrand(record)}
                     >
-                        <Button type="primary" htmlType="submit" className="danger">
+                        <Button type="primary" htmlType="submit" className="danger" style={{ marginRight: 20 }}>
                             Видалити пост
                         </Button>
                     </Popconfirm>
 
-                    <Button type="primary" htmlType="submit" className="danger">
+                    <Button type="primary" htmlType="submit" className="danger" onClick={showModalAdvertising}>
                         Рекламувати пост
                     </Button>
                 </div>
@@ -112,8 +100,7 @@ const AdminPanelPostPage = () => {
                 <Col span={12}>
                     <Input
                         placeholder="Input user id"
-                        // onChange={}
-                        //onClick={}
+                        onChange={handleSearchUserEmailChange}
                         style={{ width: "300px" }}
                     />
                 </Col>
@@ -124,26 +111,18 @@ const AdminPanelPostPage = () => {
                         className="buttonPrimary"
                         style={{ marginRight: 20 }}
                         onClick={() => {
-                            handleGetAllPostsUser();
+                            //handleGetAllPostsUser();
                         }}
                     >
                         Обновити таблицю
-                    </Button>
-                    <Button
-                        htmlType="button"
-                        type="default"
-                        className="buttonPrimary"
-                    //onClick={showModalAddNewBrand}
-                    >
-                        Рекламувати пост
                     </Button>
                 </Col>
             </Row>
 
             <FormModal
-                title="Додавання нової марки авто"
-                visible={isModalAdd}
-                onCancel={() => setModalAdd(false)}
+                title="Реклама посту"
+                visible={isModalAdvertising}
+                onCancel={() => setModalAdvertising(false)}
                 onSubmit={() => {
                     //handleOkModalAddNewBrand
                 }}>
@@ -156,9 +135,23 @@ const AdminPanelPostPage = () => {
                     form={form}
                 >
                     <Form.Item
-                        label="Назва марки"
+                        label="очок"
                         name="title"
-                        rules={[{ required: true, message: "Введіть нову марку машини" }]}
+                        //rules={[{ required: true, message: "Введіть нову марку машини" }]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        label="показів посту"
+                        name="title"
+                        //rules={[{ required: true, message: "Введіть нову марку машини" }]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        label="показів баннер"
+                        name="title"
+                        //rules={[{ required: true, message: "Введіть нову марку машини" }]}
                     >
                         <Input />
                     </Form.Item>
@@ -167,7 +160,7 @@ const AdminPanelPostPage = () => {
             <Table
                 className="adminTable"
                 size="large"
-                dataSource={paginatedBrands.data}
+                dataSource={userPosts}
                 columns={columns}
                 rowKey="id"
                 loading={loading}
--- BibikaProject.WebUI/Client/src/components/adminPanel/post/service.ts
+++ BibikaProject.WebUI/Client/src/components/adminPanel/post/service.ts
@@ -1,30 +1,33 @@
 import axios from "axios";
 import qs from "qs";
-import { stringify } from "querystring";
 import { ErrorStrings } from "../../../constants";
 import http from "../../../http_common";
 import { IRequestError } from "../types";
 
 export const getUserPostEmail = async (userEmail: string) => {
-    try {
-        const response = await http.get(`api/post/get/user-posts/?` + qs.stringify(userEmail));
-        console.log("service user posts", response.data);
-        
-        return response.data;
-    } catch (error) {
-        if (axios.isAxiosError(error)) {
-            if (error.request.status == 0 || error.request.status == 500) {
-              const unknownError: IRequestError = {
-                code: error.request.status,
-                errors: new Array<string>(ErrorStrings.backendNotResponse()),
-              };
-              throw unknownError;
-            }
-            let serverError: IRequestError = {
-              errors: error.response?.data.Errors,
-              code: error.response?.data.Code,
-            };
-            throw serverError;
-          }
+  try {
+    const response = await http.get(`api/post/get/user-posts/?` + qs.stringify(userEmail));
+    console.log("service user posts", response.data);
+
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      if (error.request.status == 0 || error.request.status == 500) {
+        const unknownError: IRequestError = {
+          code: error.request.status,
+          errors: new Array<string>(ErrorStrings.backendNotResponse()),
+        };
+        throw unknownError;
+      }
+      let serverError: IRequestError = {
+        errors: error.response?.data.Errors,
+        code: error.response?.data.Code,
+      };
+      throw serverError;
     }
-} 
+  }
+};
+
+export const deletePostUser = () => {
+
+};
